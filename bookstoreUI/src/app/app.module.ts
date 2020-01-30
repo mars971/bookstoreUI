510c6ddce77c2cfa66e3b0bf28cc35fe71c727a3
TISSOT <maxence.tissot@capgemini.com>
2020-01-30 16:04:09 +0100

    update conf

--- bookstoreUI/src/app/app.module.ts
+++ bookstoreUI/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookPageComponent } from './book-page/book-page.component';
+import { CustomerPageComponent } from './customer-page/customer-page.component';
+import { LoanPageComponent } from './loan-page/loan-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookPageComponent,
+    CustomerPageComponent,
+    LoanPageComponent
   ],
   imports: [
     BrowserModule,
