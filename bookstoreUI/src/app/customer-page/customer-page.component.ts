b2500e74d845c92630d0343c0e0f178202f2e199
TISSOT <maxence.tissot@capgemini.com>
2020-01-30 15:43:20 +0100

    add module and components

---
+++ bookstoreUI/src/app/customer-page/customer-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-page',
+  templateUrl: './customer-page.component.html',
+  styleUrls: ['./customer-page.component.less']
+})
+export class CustomerPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
