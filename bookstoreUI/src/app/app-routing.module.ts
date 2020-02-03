e3b52e57c96e7f85068be5e2bdbfb4534b142134
TISSOT <maxence.tissot@capgemini.com>
2020-02-03 16:07:02 +0100

    update component

--- bookstoreUI/src/app/app-routing.module.ts
+++ bookstoreUI/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { BookPageComponent } from './book-page/book-page.component';
+import { CustomerPageComponent } from './customer-page/customer-page.component';
+import { LoanPageComponent } from './loan-page/loan-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MenuComponent},
+  {path: 'book-page', component: BookPageComponent},
+  {path: 'customer-page', component: CustomerPageComponent},
+  {path: 'loan-page', component: LoanPageComponent},
+  ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bookstoreUI/src/app/app.module.ts
+++ bookstoreUI/src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 import { BookPageComponent } from './book-page/book-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDatepickerModule, MatInputModule,MatNativeDateModule, MAT_DATE_LOCALE} from '@angular/material';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { CustomerPageComponent } from './customer-page/customer-page.component';
 import { LoanPageComponent } from './loan-page/loan-page.component';
-import { MenuComponent } from './menu/menu.component';
+//import { BackToMenuComponent } from './menu/back-to-menu/back-to-menu.component';
+//import { MailModalComponent } from './modal/mail-modal/mail-modal.component';
+//import { MessageModalComponent } from './modal/message-modal/message-modal.component';
+
 
 @NgModule({
   declarations: [
@@ -18,9 +27,14 @@ import { MenuComponent } from './menu/menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,   /* for using form elements like NgForm */
+    HttpClientModule, /* for using http request elements end verbs like GET, POST, ... */
+    MatDatepickerModule, MatInputModule,MatNativeDateModule, BrowserAnimationsModule, ReactiveFormsModule, /* for using input date picker -> need to install @angular/material package*/
+    NgxSpinnerModule /* for using spinner */
+
   ],
-  providers: [],
+  providers: [{provide: MAT_DATE_LOCALE, useValue: 'en-GB'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
